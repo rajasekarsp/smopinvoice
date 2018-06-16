011e8c6c45afafa4993889c224220876e4ed27b7
Roar India Rio <roarindiario@gmail.com>
2018-06-16 13:05:28 +0530

    Load from Previous Invoice - Version 1

--- app/app.js
+++ app/app.js
@@ -1,65 +1,82 @@
-var invoiceApp = angular.module('myApp', [])
+var invoiceApp = angular.module('myApp', ['720kb.datepicker'])
 
-invoiceApp.controller('getDetailsController',['$scope','$http', function($scope, $http)  {
+invoiceApp.controller('getDetailsController', ['$scope', '$http', function ($scope, $http) {
 	/* DEFAULTS */
-	$scope.invoiceDate = new Date();
-	$scope.clientList = [];
-	$scope.gstType = "GST";
-	$scope.gstSlab = "18";
-	$scope.products = [];
-	$scope.goodsValue = 0;
-	$scope.packingValue = 0;
-	$scope.gstValue = 0;
-	$scope.totalAmount = 0;
-	$scope.advanceAmount = 0;
-	$scope.balanceAmount = 0;
-	$scope.preparedBy = "MSP";
-	$scope.checkedBy = "MSP";
-	$scope.refNo = "";
-	
-	$http.get("data/client-data.json").then(function(response) {
-        $scope.clientList = response.data;
+
+	$scope.invoice = {
+		"invoiceDate":"",
+		"invoiceNo":"",
+		"eWayBillNo":"",
+		"gstType": "GST",
+		"gstSlab": "18",
+		"products": [],
+		"goodsValue": 0,
+		"packingValue": 0,
+		"gstValue": 0,
+		"totalAmount": 0,
+		"advanceAmount": 0,
+		"balanceAmount": 0,
+		"preparedBy": "MSP",
+		"checkedBy": "MSP",
+		"refNo":"",
+		"despatchedThru":"",
+		"lrNo":"",
+		"despatchedDate":"",
+		"despatchedTo":"",
+		"documentsThru":"",
+		"invoiceOrgType":"Original",
+		"client":{
+			"clientName":"",
+			"clientAddress1":"",
+			"clientAddress2":"",
+			"clientCity":"",
+			"clientPincode":"",
+			"clientGstNo":""
+		}
+	};
+
+	$http.get("data/client-data.json").then(function (response) {
+		$scope.clientList = response.data;
 	});
 
-	$scope.changeClient = function(){
-		if($scope.selectedClient){
-			$scope.clientName = $scope.selectedClient.name;
-			$scope.clientAddress1 = $scope.selectedClient.address1;
-			$scope.clientAddress2 = $scope.selectedClient.address2;
-			$scope.clientCity = $scope.selectedClient.city;
-			$scope.clientPincode = $scope.selectedClient.pincode;
-			$scope.clientGstNo = $scope.selectedClient.gstNo;
+	$scope.changeClient = function () {
+		if ($scope.selectedClient) {
+			$scope.invoice.client.clientName = $scope.selectedClient.name;
+			$scope.invoice.client.clientAddress1 = $scope.selectedClient.address1;
+			$scope.invoice.client.clientAddress2 = $scope.selectedClient.address2;
+			$scope.invoice.client.clientCity = $scope.selectedClient.city;
+			$scope.invoice.client.clientPincode = $scope.selectedClient.pincode;
+			$scope.invoice.client.clientGstNo = $scope.selectedClient.gstNo;
 		}
-		else{
-			$scope.clientName = null;
-			$scope.clientAddress1 = null;
-			$scope.clientAddress2 = null;
-			$scope.clientCity = null;
-			$scope.clientPincode = null;
-			$scope.clientGstNo = null;
+		else {
+			$scope.invoice.client.clientName = null;
+			$scope.invoice.client.clientAddress1 = null;
+			$scope.invoice.client.clientAddress2 = null;
+			$scope.invoice.client.clientCity = null;
+			$scope.invoice.client.clientPincode = null;
+			$scope.invoice.client.clientGstNo = null;
 		}
 	}
 
-$scope.invoiceOrgType = 'Original';
+	
 	$scope.preview = false;
 	/* PRODUCTS */
-	$scope.products.push(new Product());
-	$scope.addProduct = function(){
-		$scope.products.push(new Product());
+	$scope.invoice.products.push(new Product());
+	$scope.addProduct = function () {
+		$scope.invoice.products.push(new Product());
 	}
 
-	$scope.removeProduct = function(product){
-		console.log($scope.products.indexOf(product));
-		$scope.products.splice($scope.products.indexOf(product), 1);
+	$scope.removeProduct = function (product) {
+		$scope.invoice.products.splice($scope.invoice.products.indexOf(product), 1);
 	}
 
-	$scope.updateAmount = function(product){
+	$scope.updateAmount = function (product) {
 		product.amount = parseFloat(((product.rate * product.quantity) / product.ratePer).toFixed(2));
 	}
-	var ones = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
-	var tens = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
-	
-	$scope.wordsConvertor = function(num){
+	var ones = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen '];
+	var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+	$scope.wordsConvertor = function (num) {
 		if ((num = num.toString()).length > 9) return 'overflow';
 		n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
 		if (!n) return; var str = '';
@@ -70,30 +87,40 @@ $scope.invoiceOrgType = 'Original';
 		str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (ones[Number(n[5])] || tens[n[5][0]] + ' ' + ones[n[5][1]]) + 'only ' : 'only';
 		return str;
 	}
-	
-	$scope.$watch(
-		function(scope) { 
-			$scope.goodsValue = 0;
-			angular.forEach($scope.products, function(product) {
-				$scope.goodsValue += product.amount;
-			});
-			$scope.goodsValue += $scope.packingValue;
-			$scope.gstValue = parseFloat(( ($scope.goodsValue + $scope.packingValue) * $scope.gstSlab / 100).toFixed(2));
-			$scope.totalAmount = parseFloat(($scope.goodsValue + $scope.packingValue + $scope.gstValue).toFixed(2));
-			$scope.balanceAmount = parseFloat(($scope.totalAmount - $scope.advanceAmount).toFixed(0));			
-		}
-   );
 
-   $scope.swapView = function(arg){
+	$scope.$watch(
+		function (scope) {
+			$scope.invoice.goodsValue = 0;
+			angular.forEach($scope.invoice.products, function (product) {
+				$scope.invoice.goodsValue += product.amount;
+			});
+			$scope.invoice.goodsValue += $scope.invoice.packingValue;
+			$scope.invoice.gstValue = parseFloat((($scope.invoice.goodsValue + $scope.invoice.packingValue) * $scope.invoice.gstSlab / 100).toFixed(2));
+			$scope.invoice.totalAmount = parseFloat(($scope.invoice.goodsValue + $scope.invoice.packingValue + $scope.invoice.gstValue).toFixed(2));
+			$scope.invoice.balanceAmount = parseFloat(($scope.invoice.totalAmount - $scope.invoice.advanceAmount).toFixed(0));
+		}
+	);
+
+	$scope.swapView = function (arg) {
 		window.scrollTo(0, 0);
 		$scope.preview = (arg == 'preview') ? true : false;
-   }
+	}
 
-   $scope.print = function(){
-	   window.print();
-   }
+	$scope.print = function () {
+		localStorage.setItem("prevInvoiceData", angular.toJson($scope.invoice));
+		window.print();
+	}
 
-
-
+	$scope.loadPrevInvoice = function(){
+		var prevData =  localStorage.getItem("prevInvoiceData");
+		if(prevData){
+			$scope.invoice = JSON.parse(prevData);
+			$scope.invoice.invoiceOrgType = "Copy";
+		}
+		else{
+			alert("Sorry! Old Invoice not available");
+		}
+		
+	}
 
 }])
